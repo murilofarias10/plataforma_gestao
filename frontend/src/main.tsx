aff4a39e7a836e416076fb3d1929b052d1a20bbb
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2025-11-26 19:54:06 -0800

    changing font for monthserra library

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "@fontsource/montserrat/300.css"; // Light
+import "@fontsource/montserrat/400.css"; // Regular
+import "@fontsource/montserrat/500.css"; // Medium
+import "@fontsource/montserrat/600.css"; // Semi-bold
+import "@fontsource/montserrat/700.css"; // Bold
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
